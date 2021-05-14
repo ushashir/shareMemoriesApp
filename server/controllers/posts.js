45b49f2e09ea3fc6c457e5f02233f635e207649f
Ushahemba Shir <laleoict@gmail.com>
2021-05-14 15:13:31 +0100

    setCurrentId not a function bug fixed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,8 +7,6 @@ export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
 
-        // console.log(PostMessages);
-
         res.status(200).json(postMessages)
 
     } catch (error) {
