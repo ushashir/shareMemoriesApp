d6d8ae3c23f1f2b0391f12e105c9414049fd4f4b
Ushahemba Shir <laleoict@gmail.com>
2021-05-27 15:07:38 +0100

    first commit

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
-import Post from './Post/Post';
+import Post from './post/Post';
 import useStyles from './style';
 
 const Posts = ({ setCurrentId }) => {
